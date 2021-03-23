4d09d3d14c343e14c13ab1cbaf29e66e31a628bd
Ruth Dooley <ruthdooley@Ruths-MacBook-Pro-2.local>
2021-03-23 14:32:12 +0000

    singly linked list

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -48,9 +48,7 @@ public class SinglyLinkedList<E> implements Cloneable, Iterable<E>, List<E> {
      *
      * @return true if the linked list is empty, false otherwise
      */
-    public boolean isEmpty() {
-        return size == 0;
-    }
+    public boolean isEmpty() { return size == 0; }
 
     @Override
     public E get(int i) throws IndexOutOfBoundsException {
@@ -71,45 +69,48 @@ public class SinglyLinkedList<E> implements Cloneable, Iterable<E>, List<E> {
     @Override
     //Set a node in index i in the linked list to value of e?
     public E set(int i, E e) throws IndexOutOfBoundsException {
-
+        Node<E> prev = head;
+        for (int x = 0; x < i; x++){
+            prev = prev.next;
+        }
+        prev.data = e;
         return null;
     }
 
     @Override
     public void add(int i, E e) throws IndexOutOfBoundsException {
-//        Node<E> newNode = new Node<>(e, null);
-//        Node<E> prev = head;
-
-        if (i == 0) {
-            addFirst(e);
-            return;
+        Node<E> newest = new Node<>(e, null);
+        Node<E> prev = head;
+        for (int x = 1; x < i; x++ ){
+            prev = prev.next;
         }
-
-        Node<E> curr = head;
-        Node<E> prev = null;
-
-        for(int k = 0; k < i; k++) {
-            prev = curr;
-            curr = curr.next;
-        }
-
-        Node<E> newest = new Node<E>(e, curr.next);
-        //insert in between the two nodes either side of i
+        Node<E> current = prev.next;
+        newest.next = current;
         prev.next = newest;
-        newest.next = curr;
         size++;
-
-
-
-//        for (int x = 1; x < i; x++){ prev = prev.next; }
-//        Node<E> current = prev.next;
-//        newNode.next = current;
-//        prev.next = newNode;
     }
 
     @Override
     public E remove(int i) throws IndexOutOfBoundsException {
-        return null;
+        if (i == 0) {
+            E e = removeFirst();
+            return e;
+        }
+
+        Node<E> curr = head;
+        Node<E> prev = null;
+        int x = 0;
+
+        while (curr != null && x != i) {
+            prev = curr;
+            curr = curr.next;
+            x++;
+        }
+
+        E e = prev.getData();
+        prev.next = curr.next;
+        size--;
+        return e;
     }
 
     /**
@@ -118,8 +119,11 @@ public class SinglyLinkedList<E> implements Cloneable, Iterable<E>, List<E> {
      * @return element at the front of the list (or null if empty)
      */
     public E first() {
-        // TODO
-        return null;
+        if (head == null){
+            return null;
+        } else {
+            return head.getData();
+        }
     }
 
     /**
@@ -128,8 +132,18 @@ public class SinglyLinkedList<E> implements Cloneable, Iterable<E>, List<E> {
      * @return last node of the list (or null if empty)
      */
     public Node<E> getLast() {
-        // TODO
-        return null;
+        if (head == null){
+            return null;
+        }
+
+        Node <E> curr = head;
+        Node <E> prev = null;
+        while (curr.next != null){
+            curr = curr.next;
+            prev = curr;
+        }
+        Node<E> e = prev;
+        return e;
     }
 
     /**
@@ -138,8 +152,17 @@ public class SinglyLinkedList<E> implements Cloneable, Iterable<E>, List<E> {
      * @return element at the end of the list (or null if empty)
      */
     public E last() {
-        // TODO
-        return null;
+        if (head == null){
+            return null;
+        }
+
+        Node <E> curr = head;
+        Node <E> prev = null;
+        while (curr.next != null){
+            curr = curr.next;
+            prev = curr;
+        }
+        return prev.getData();
     }
 
     // update methods
@@ -173,7 +196,26 @@ public class SinglyLinkedList<E> implements Cloneable, Iterable<E>, List<E> {
     }
 
     public E removeLast() {
-        return null;
+        if (head == null){
+            throw new IndexOutOfBoundsException("List is empty");
+        } else if (head.getNext() == null){
+            size--;
+            E e = head.getData();
+            head = null;
+            return e;
+        }
+
+        Node<E> curr = head;
+        Node<E> prev = null;
+        while (curr.next != null){
+            prev = curr;
+            curr = curr.getNext();
+        }
+
+        E e = curr.getData();
+        prev.next = null;
+        size--;
+        return e;
     }
 
     /**
@@ -182,8 +224,17 @@ public class SinglyLinkedList<E> implements Cloneable, Iterable<E>, List<E> {
      * @return the removed element (or null if empty)
      */
     public E removeFirst() {
-        // TODO
-        return null;
+        if (size() == 1) {
+            E e = head.getData();
+            head = null;
+            size--;
+            return e;
+        } else {
+            E temp = head.getData();
+            head = head.getNext();
+            size--;
+            return temp;
+        }
     }
 
     @SuppressWarnings({"unchecked"})
@@ -216,16 +267,22 @@ public class SinglyLinkedList<E> implements Cloneable, Iterable<E>, List<E> {
     }
 
     private class SinglyLinkedListIterator<E> implements Iterator<E> {
+        Node<E> curr;
+
+        public void ListIterator(){
+            curr = (Node<E>) head;
+        }
+
         @Override
         public boolean hasNext() {
-            // TODO
-            return false;
+            return curr != null;
         }
 
         @Override
         public E next() {
-            // TODO
-            return null;
+            E next = (E) curr.getData();
+            curr = curr.getNext();
+            return next;
         }
     }
 
@@ -235,6 +292,10 @@ public class SinglyLinkedList<E> implements Cloneable, Iterable<E>, List<E> {
 
     public static void main(String[] args) {
         SinglyLinkedList<Integer> ll = new SinglyLinkedList<Integer>();
+
+        //Test: isEmpty Function
+        if (ll.isEmpty()){ System.out.println("Linked list is empty");
+        } else { System.out.println("Linked list is not empty"); }
 
         //Test: Check addFirst method and toString
         ll.addFirst(1);
@@ -247,21 +308,38 @@ public class SinglyLinkedList<E> implements Cloneable, Iterable<E>, List<E> {
         System.out.println(ll.toString()); // 0, 1, 2, 3
 
         //Test: Check add function
-        ll.add(5, 3);
-        ll.add(6, 4);
+        ll.add(4, 4);
+        ll.add(5, 5);
         System.out.println(ll.toString()); // 0, 1, 2, 3, 4, 5
 
+        //Test: Check size and isEmpty Function for false
+        System.out.println(ll.size()); //6
+        if (ll.isEmpty()){ System.out.println("Linked list is empty");
+        } else { System.out.println("Linked list is not empty"); }
 
-//        String[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-//
-//        for (String s : alphabet) {
-//            sll.addFirst(s);
-//            sll.addLast(s);
-//        }
-//        System.out.println(sll.toString());
-//
-//        for (String s : sll) {
-//            System.out.print(s + ", ");
-//        }
+        //Test: Check set function
+        ll.set(0, 1);
+        ll.set(1, 2);
+        ll.set(2, 3);
+        ll.set(3, 4);
+        ll.set(4, 5);
+        ll.set(5, 6);
+        System.out.println(ll.toString()); // 1, 2, 3, 4, 5, 6
+
+        //Test: Check remove function and removeFirst
+        ll.remove(1);
+        System.out.println(ll.toString()); // 1, 3, 4, 5, 6
+        ll.removeFirst();
+        System.out.println(ll.toString()); // 3, 4, 5, 6
+
+        //Test: Check removeLast function
+        ll.removeLast();
+        ll.removeLast();
+        System.out.println(ll.toString()); // 3, 4
+
+        //Test: Check first and last methods and getLast
+        System.out.println(ll.first()); //3
+        System.out.println(ll.last()); //4
+        System.out.println(ll.getLast());
     }
 }
