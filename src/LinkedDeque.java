19754ce84eb89dcc871cdbb5164edfd2fbf83248
Ruth Dooley <ruthdooley@Ruths-MacBook-Pro-2.local>
2021-04-28 23:29:06 +0100

    deque not tested

---
+++ src/LinkedDeque.java
@@ -0,0 +1,51 @@
+public class LinkedDeque<E> implements Deque<E> {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+    DoublyLinkedList<E> dll;
+    LinkedDeque() {
+        dll = new DoublyLinkedList<E>();
+    }
+
+    @Override
+    public int size() {
+        return dll.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return dll.isEmpty();
+    }
+
+    @Override
+    public E first() {
+        return dll.get(0);
+    }
+
+    @Override
+    public E last() {
+        return dll.get(dll.size()-1);
+    }
+
+    @Override
+    public void addFirst(E e) {
+        dll.addFirst(e);
+    }
+
+    @Override
+    public void addLast(E e) {
+        dll.addFirst(e);
+    }
+
+    @Override
+    public E removeFirst() {
+        return dll.removeFirst();
+    }
+
+    @Override
+    public E removeLast() {
+        return dll.removeLast();
+    }
+}
