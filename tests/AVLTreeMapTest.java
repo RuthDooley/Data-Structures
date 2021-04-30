4d4f4cda780854bdb107262c24ff06d7942910eb
Ruth Dooley <ruthdooley@Ruths-MacBook-Pro-2.local>
2021-04-30 18:41:32 +0100

    avl tree map put test complete

--- tests/AVLTreeMapTest.java
+++ tests/AVLTreeMapTest.java
@@ -23,8 +23,7 @@ class AVLTreeMapTest {
         for(Integer i : arr) {
             map.put(i, Integer.toString(i));
         }
-
-        assertEquals("[1, 2, 4, 5, 12, 15, 21, 23, 24, 26, 33, 35]", map.keySet().toString());
+        assertEquals("[1, 2, 4, 5, 12, 15, 21, 23, 24, 26, 33, 35]", map.toString());
     }
 
     @Test
